30fdf3ff4f54356b592f132f812ac11e8932dcf2
jamaldemir <jamal@cybertekschool.com>
2021-08-15 15:50:13 -0400

    Hello test case done

--- src/test/java/com/cybertek/day2/SpartanGetRequests.java
+++ src/test/java/com/cybertek/day2/SpartanGetRequests.java
@@ -67,4 +67,39 @@ public class SpartanGetRequests {
 
     }
 
+    /*
+        Given no headers provided
+        When Users sends GET request to /api/hello
+        Then response status code should be 200
+        And Content type header should be “text/plain;charset=UTF-8”
+        And header should contain date
+        And Content-Length should be 17
+        And body should be “Hello from Sparta"
+        */
+
+    @DisplayName("GET request to /api/hello")
+    @Test
+    public void test3(){
+        //send request and save response inside the response object
+        Response response = RestAssured.when().get(baseUrl + "/api/hello");
+
+        //verify status code 200
+        Assertions.assertEquals(200,response.statusCode());
+
+        //verify content type
+        Assertions.assertEquals("text/plain;charset=UTF-8",response.contentType());
+
+        //verify we have headers named date
+        //we use hasHeaderWithname method to verify header exist or not
+        Assertions.assertTrue(response.headers().hasHeaderWithName("Date"));
+        //how to get and header from response using header key ?
+        //we use response.header(String headerName) method to get any header value
+        System.out.println("response.header(\"Content-Length\") = " + response.header("Content-Length"));
+        System.out.println(response.header("Date"));
+
+        //verify content length is 17
+        Assertions.assertEquals("17",response.header("Content-Length"));
+        //verify body is "Hello from Sparta"
+        Assertions.assertEquals("Hello from Sparta",response.body().asString());
+    }
 }
