d3d221cb384fc410d15dc637327418bf8f3517a4
jamaldemir <jamal@cybertekschool.com>
2021-08-15 16:20:59 -0400

    static import implemented

--- src/test/java/com/cybertek/day2/HrGetRequests.java
+++ src/test/java/com/cybertek/day2/HrGetRequests.java
@@ -1,11 +1,15 @@
 package com.cybertek.day2;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HrGetRequests {
 
@@ -13,14 +17,14 @@ public class HrGetRequests {
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
-        RestAssured.baseURI = "http://52.207.61.129:1000/ords/hr";
+        baseURI = "http://52.207.61.129:1000/ords/hr";
     }
 
     @DisplayName("GET request to /regions")
     @Test
     public void test1(){
 
-        Response response = RestAssured.get("/regions");
+        Response response = get("/regions");
 
         //print the status code
         System.out.println(response.statusCode());
@@ -38,17 +42,19 @@ public class HrGetRequests {
     @DisplayName("GET request to /regions/2")
     @Test
     public void test2(){
-        Response response = RestAssured.get("/regions/2");
+        Response response = given().accept(ContentType.JSON)
+                            .when()
+                                    .get("/regions/2");
 
         //verify status code
-        Assertions.assertEquals(200,response.statusCode());
+        assertEquals(200,response.statusCode());
         //verify content type
-        Assertions.assertEquals("application/json",response.contentType());
+        assertEquals("application/json",response.contentType());
 
         response.prettyPrint();
 
         //verify body contains Americas
-        Assertions.assertEquals(response.body().asString().contains("Americas"),true);
+        assertTrue(response.body().asString().contains("Americas"));
 
 
 
