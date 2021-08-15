30060e3df7acf0f4396f5215452eedc42d7045f0
jamaldemir <jamal@cybertekschool.com>
2021-08-15 15:15:03 -0400

    second test case done

--- src/test/java/com/cybertek/day2/SpartanGetRequests.java
+++ src/test/java/com/cybertek/day2/SpartanGetRequests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequests {
@@ -39,13 +40,31 @@ public class SpartanGetRequests {
         Assertions.assertEquals(response.contentType(),"application/json");
 
     }
-    
+
      /*
-        Given accept is application/json
+        Given accept header is application/json
         When users sends a get request to /api/spartans/3
         Then status code should be 200
         And content type should be application/json
         and json body should contain Fidole
      */
+
+    @DisplayName("GET one spartan /api/spartans/3 and verify")
+    @Test
+    public void test2(){
+        Response response = RestAssured.given().accept(ContentType.JSON).
+                when().get(baseUrl + "/api/spartans/3");
+
+
+        //verify status code 200
+        Assertions.assertEquals(200,response.statusCode());
+
+        //verify content type
+        Assertions.assertEquals("application/json",response.contentType());
+
+        //verify json body contains Fidole
+        Assertions.assertTrue(response.body().asString().contains("Fidole"));
+
+    }
 
 }
