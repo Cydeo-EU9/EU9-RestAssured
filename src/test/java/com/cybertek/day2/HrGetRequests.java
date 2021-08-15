0d206adfebbc65e7c6800271787322dd47c07679
jamaldemir <jamal@cybertekschool.com>
2021-08-15 16:06:58 -0400

    Task added to HR

--- src/test/java/com/cybertek/day2/HrGetRequests.java
+++ src/test/java/com/cybertek/day2/HrGetRequests.java
@@ -1,15 +1,40 @@
 package com.cybertek.day2;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class HrGetRequests {
 
     //BeforeAll is a annotation equals to @BeforeClass in testNg, we use with static method name
     @BeforeAll
     public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
         RestAssured.baseURI = "http://52.207.61.129:1000/ords/hr";
     }
+
+    @DisplayName("GET request to /regions")
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.get("/regions");
+
+        //print the status code
+        System.out.println(response.statusCode());
+
+    }
+
+     /*
+        Given accept type is application/json
+        When user sends get request to /regions/2
+        Then response status code must be 200
+        and content type equals to application/json
+        and response body contains Americas
+     */
+
+
 
 
 
