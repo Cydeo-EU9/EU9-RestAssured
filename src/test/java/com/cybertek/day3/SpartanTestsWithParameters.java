aeeafe08a0d8cb80a9e3287ca96b96408b934377
jamaldemir <jamal@cybertekschool.com>
2021-08-17 19:27:06 -0400

    Day3:pathParam test1

--- src/test/java/com/cybertek/day3/SpartanTestsWithParameters.java
+++ src/test/java/com/cybertek/day3/SpartanTestsWithParameters.java
@@ -1,6 +1,10 @@
 package com.cybertek.day3;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
@@ -14,5 +18,33 @@ public class SpartanTestsWithParameters {
         baseURI = "http://52.207.61.129:8000";
     }
 
-    
+     /*   Given accept type is Json
+          And Id parameter value is 5
+          When user sends GET request to /api/spartans/{id}
+          Then response status code should be 200
+          And response content-type: application/json
+          And "Blythe" should be in response payload
+       */
+
+    @DisplayName("GET request to /api/spartans/{id} with ID 5")
+    @Test
+    public void test1(){
+
+        Response response = given().
+                                      accept(ContentType.JSON)
+                                      .and().pathParam("id", 5)
+                            .when()
+                                     .get("/api/spartans/{id}");
+        //verify status code
+        assertEquals(200,response.statusCode());
+        //verify content type
+        assertEquals("application/json",response.contentType());
+        //verify Blythe in the json payload/body
+        assertTrue(response.body().asString().contains("Blythe"));
+
+
+
+    }
+
+
 }
