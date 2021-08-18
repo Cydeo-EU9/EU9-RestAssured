3c92807233d33d5ca50ec38e873c22975ec54493
jamaldemir <jamal@cybertekschool.com>
2021-08-17 21:36:42 -0400

    Day3:SpartanTestWithPath test1

--- src/test/java/com/cybertek/day3/SpartanTestsWithPath.java
+++ src/test/java/com/cybertek/day3/SpartanTestsWithPath.java
@@ -1,5 +1,7 @@
 package com.cybertek.day3;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,6 +33,34 @@ public class SpartanTestsWithPath {
     @DisplayName("GET one spartan with Path Method")
     @Test
     public void test1(){
+    Response response = given().accept(ContentType.JSON)
+                            .and().pathParam("id",10)
+                    .when().get("/api/spartans/{id}");
+
+    assertEquals(200,response.statusCode());
+    assertEquals("application/json",response.contentType());
+
+    //verify each json key has specific value
+        System.out.println(response.path("id").toString());
+        System.out.println(response.path("name").toString());
+        System.out.println(response.path("gender").toString());
+        System.out.println(response.path("phone").toString());
+
+        int id = response.path("id");
+        String name = response.path("name");
+        String gender = response.path("gender");
+        long phone = response.path("phone");
+
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("gender = " + gender);
+        System.out.println("phone = " + phone);
+
+        //assert the values
+        assertEquals(10,id);
+        assertEquals("Lorenza",name);
+        assertEquals("Female",gender);
+        assertEquals(3312820936l,phone);
 
 
     }
