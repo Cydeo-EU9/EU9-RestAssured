4fa083081d58a4c993e0d7a495410b769d387aee
jamaldemir <jamal@cybertekschool.com>
2021-08-17 20:19:27 -0400

    Day3:pathParam test4 query param with map

--- src/test/java/com/cybertek/day3/SpartanTestsWithParameters.java
+++ src/test/java/com/cybertek/day3/SpartanTestsWithParameters.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -95,6 +98,33 @@ public class SpartanTestsWithParameters {
                               .and().queryParam("gender","Female")
                 .when()
                         .get("/api/spartans/search");
+
+        //verify status code 200
+        assertEquals(200,response.statusCode());
+        //verify content type
+        assertEquals("application/json",response.contentType());
+        //verify NotFound in the json payload/body
+
+        //"Female" should be in response payload
+        assertTrue(response.body().asString().contains("Female"));
+        //"Janette" should be in response payload
+        assertTrue(response.body().asString().contains("Janette"));
+
+    }
+    @DisplayName("GET request to /api/spartans/search with Query Params (MAP)")
+    @Test
+    public void test4(){
+        //create a map and add query parameters
+        Map<String,Object> queryMap = new HashMap<>();
+        queryMap.put("nameContains","e");
+        queryMap.put("gender","Female");
+
+        Response response = given().
+                                    log().all()
+                                    .accept(ContentType.JSON)
+                                    .and().queryParams(queryMap)
+                            .when()
+                                    .get("/api/spartans/search");
 
         //verify status code 200
         assertEquals(200,response.statusCode());
