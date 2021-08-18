8a06e08d13e5307daf91510bf3d0f15e4259b7f5
jamaldemir <jamal@cybertekschool.com>
2021-08-17 21:21:36 -0400

    Day3:SpartanTestWithPath class added

---
+++ src/test/java/com/cybertek/day3/SpartanTestsWithPath.java
@@ -0,0 +1,37 @@
+package com.cybertek.day3;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanTestsWithPath {
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://52.207.61.129:8000";
+    }
+
+    /*
+     Given accept type is json
+     And path param id is 10
+     When user sends a get request to "api/spartans/{id}"
+     Then status code is 200
+     And content-type is "application/json"
+     And response payload values match the following:
+          id is 10,
+          name is "Lorenza",
+          gender is "Female",
+          phone is 3312820936
+   */
+    @DisplayName("GET one spartan with Path Method")
+    @Test
+    public void test1(){
+
+
+    }
+}
