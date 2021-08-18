52596c305a34fae498b24c8e0fa58be060604819
jamaldemir <jamal@cybertekschool.com>
2021-08-17 21:50:49 -0400

    Day3:SpartanTestWithPath test2

--- src/test/java/com/cybertek/day3/SpartanTestsWithPath.java
+++ src/test/java/com/cybertek/day3/SpartanTestsWithPath.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -63,5 +65,31 @@ public class SpartanTestsWithPath {
         assertEquals(3312820936l,phone);
 
 
+    }
+
+    @DisplayName("GET all spartan and navigate with Path()")
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/api/spartans");
+
+        //response.prettyPrint();
+
+        int firstId = response.path("id[0]");
+        System.out.println("firstId = " + firstId);
+
+        String name = response.path("name[0]");
+        System.out.println("name = " + name);
+
+        String lastFirstName = response.path("name[-2]");
+        System.out.println("lastFirstName = " + lastFirstName);
+
+        //save names inside the list of string
+        List<String> names = response.path("name");
+        System.out.println(names);
+        //print each name one by one
+        for (String n : names) {
+            System.out.println(n);
+        }
     }
 }
