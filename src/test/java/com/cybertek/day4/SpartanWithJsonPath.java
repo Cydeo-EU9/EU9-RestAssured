9ef12552e06b200d7f364755f27630aa6fea3015
jamaldemir <jamal@cybertekschool.com>
2021-08-18 20:33:34 -0400

    Day4: SpartanWithJsonPath added

---
+++ src/test/java/com/cybertek/day4/SpartanWithJsonPath.java
@@ -0,0 +1,22 @@
+package com.cybertek.day4;
+
+import com.cybertek.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanWithJsonPath extends SpartanTestBase {
+
+    @DisplayName("GET one spartan with JsonPath")
+    @Test
+    public void test1(){
+
+
+
+
+    }
+}
