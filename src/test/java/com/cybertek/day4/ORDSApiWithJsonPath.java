294db7f3847d621d918ceb1fe769845ead9e5377
jamaldemir <jamal@cybertekschool.com>
2021-08-18 20:59:19 -0400

    Day4: ORDSApiWithJsonPath added

--- src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
@@ -3,6 +3,7 @@ package com.cybertek.day4;
 import com.cybertek.utilities.HRTestBase;
 import com.cybertek.utilities.HRTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,23 @@ public class ORDSApiWithJsonPath extends HRTestBase {
     @DisplayName("GET request to Countries")
     @Test
     public void test1(){
+
+        Response response = get("/countries");
+
+        //get the second country name with JsonPath
+
+        //to use jsonpath we assign response to JsonPath
+        JsonPath jsonPath = response.jsonPath();
+
+        String secondCountryName = jsonPath.getString("items[1].country_name");
+        System.out.println("secondCountryName = " + secondCountryName);
+
+        //get all country ids
+        //items.country_id
+        List<String> allCountryIds = jsonPath.getList("items.country_id");
+        System.out.println(allCountryIds);
+
+
 
     }
 }
