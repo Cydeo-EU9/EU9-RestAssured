54ef6d88486b3735316ab12e6cc0b828be003aaa
jamaldemir <jamal@cybertekschool.com>
2021-08-18 21:56:58 -0400

    Day4: CBTrainingApiWithJsonPath.java added

---
+++ src/test/java/com/cybertek/day4/CBTrainingApiWithJsonPath.java
@@ -0,0 +1,39 @@
+package com.cybertek.day4;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class CBTrainingApiWithJsonPath {
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://api.cybertektraining.com";
+
+    }
+
+    @DisplayName("GET Request to individual student")
+    @Test
+    public void test1(){
+        //send a get request to student id 23401 as a path parameter and accept header application/json
+        //verify status code=200 /content type=application/json;charset=UTF-8 /Content-Encoding = gzip
+        //verify Date header exists
+        //assert that
+            /*
+                firstName Vera
+                batch 14
+                section 12
+                emailAddress aaa@gmail.com
+                companyName Cybertek
+                state IL
+                zipCode 60606
+
+                using JsonPath
+             */
+
+
+    }
+}
