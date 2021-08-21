cdd780a8f9a171ccbef2de811dbb0df21e3728c7
jamaldemir <jamal@cybertekschool.com>
2021-08-21 11:27:45 -0400

    Day5: HamcrestMatchersApiTest.java first test case done

--- src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -1,6 +1,7 @@
 package com.cybertek.day5;
 
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +10,39 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class HamcrestMatchersApiTest {
+    /*
+       given accept type is Json
+       And path param id is 15
+       When user sends a get request to spartans/{id}
+       Then status code is 200
+       And content type is Json
+       And json data has following
+           "id": 15,
+           "name": "Meta",
+           "gender": "Female",
+           "phone": 1938695106
+        */
 
     @DisplayName("OneSpartan with Hamcrest and chaining")
     @Test
     public void test1(){
+
+            given().
+                    accept(ContentType.JSON)
+                    .and().pathParam("id",15)
+            .when()
+                    .get("http://52.207.61.129:8000/api/spartans/{id}")
+            .then()
+                    .statusCode(200)
+                    .and().assertThat()
+                    .contentType("application/json")
+                    .and()
+                    .body("id",equalTo(15),
+       "name",is("Meta"),
+                               "gender",is("Female"),
+                               "phone",is(1938695106));
+
+
 
 
 
