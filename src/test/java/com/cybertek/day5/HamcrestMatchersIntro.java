f17186fd45a6a64808bae7b435dc0a5cddae7b80
jamaldemir <jamal@cybertekschool.com>
2021-08-21 10:19:17 -0400

    Day5: HamcrestMatchersIntro.java created

--- src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
@@ -1,12 +1,18 @@
 package com.cybertek.day5;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class HamcrestMatchersIntro {
 
     @Test
     public void simpleTest1(){
 
+        assertThat(5+5, is(10));
 
 
 
