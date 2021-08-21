9235dfa1f49f8f4dd4a1714a5f71c4edee9358c5
jamaldemir <jamal@cybertekschool.com>
2021-08-21 14:12:48 -0400

    Day5: SpartanHamcrestTest.java added

---
+++ src/test/java/com/cybertek/day5/SpartanHamcrestTest.java
@@ -0,0 +1,19 @@
+package com.cybertek.day5;
+
+import com.cybertek.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanHamcrestTest extends SpartanTestBase {
+
+    @DisplayName("GET spartan/search and chaining together")
+    @Test
+    public void test1(){
+
+
+    }
+}
