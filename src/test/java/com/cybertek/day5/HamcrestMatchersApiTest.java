d07c3d5f0ec63d8691a226c8c3d9694ec7b7d0e3
jamaldemir <jamal@cybertekschool.com>
2021-08-21 11:56:47 -0400

    Day5: HamcrestMatchersApiTest.java third is done

--- src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -68,6 +68,23 @@ public class HamcrestMatchersApiTest {
 
     }
 
+    @DisplayName("GET request to teacher/all and chaining")
+    @Test
+    public void teachersTest(){
+
+        //verify Alexander,Darleen,Sean inside the all teachers
+        given()
+                .accept(ContentType.JSON)
+        .when()
+                .get("http://api.cybertektraining.com/teacher/all")
+        .then()
+                .statusCode(200)
+                .and()
+                .body("teachers.firstName",hasItems("Alexander","Darleen","Sean"));
+
+
+    }
+
 
 
 }
