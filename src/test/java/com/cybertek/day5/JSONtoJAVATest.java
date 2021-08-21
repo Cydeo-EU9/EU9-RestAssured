b0c726c451e373c8fe915dfe86e441b64738aa06
jamaldemir <jamal@cybertekschool.com>
2021-08-21 14:39:53 -0400

    Day5: JSONtoJAVATest.java addded

--- src/test/java/com/cybertek/day5/JSONtoJAVATest.java
+++ src/test/java/com/cybertek/day5/JSONtoJAVATest.java
@@ -1,8 +1,11 @@
 package com.cybertek.day5;
 
 import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,9 +16,13 @@ public class JSONtoJAVATest extends SpartanTestBase {
     @Test
     public void oneSpartanToMap(){
 
+        Response response = given().pathParam("id", 15)
+                            .when().get("/api/spartans/{id}")
+                            .then().statusCode(200).extract().response();
 
+        //get the json and convert it to the map
 
-
+        Map<String,Object> jsonMap = response.as(Map.class);
 
 
     }
