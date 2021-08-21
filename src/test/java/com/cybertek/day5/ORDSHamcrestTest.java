129c342c828a043a430dfee7729e4e179a439c4a
jamaldemir <jamal@cybertekschool.com>
2021-08-21 12:01:10 -0400

    Day5: ORDSHamcrestTest.java added

---
+++ src/test/java/com/cybertek/day5/ORDSHamcrestTest.java
@@ -0,0 +1,21 @@
+package com.cybertek.day5;
+
+import com.cybertek.utilities.HRTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ORDSHamcrestTest extends HRTestBase {
+
+
+
+    @DisplayName("GET request to Employees IT_PROG endpoint and chaining")
+    @Test
+    public void employeesTest(){
+        //send a get request to emplyoees endpoint with query parameter job_id IT_PROG
+        //verify each job_id is IT_PROG
+        //verify first names are .... (find proper method to check list against list)
+        //verify emails without checking order (provide emails in different order,just make sure it has same emails)
+
+
+    }
+}
