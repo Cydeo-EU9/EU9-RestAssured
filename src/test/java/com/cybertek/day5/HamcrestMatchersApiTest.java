88e956642cf70a83dfc78b9cfa3e78feedd4ed57
jamaldemir <jamal@cybertekschool.com>
2021-08-21 10:54:28 -0400

    Day5: HamcrestMatchersApiTest.java added

---
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -0,0 +1,22 @@
+package com.cybertek.day5;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HamcrestMatchersApiTest {
+
+    @DisplayName("OneSpartan with Hamcrest and chaining")
+    @Test
+    public void test1(){
+
+
+
+
+    }
+
+}
