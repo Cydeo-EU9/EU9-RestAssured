9729ceb1d7786708c4e87e426a254747c4ad6c1c
jamaldemir <jamal@cybertekschool.com>
2021-08-22 14:58:43 -0400

    Day6: Employee.java added @JsonIgnoreProperties done

--- src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -40,4 +40,13 @@ public class ORDSPojoGetRequestTest extends HRTestBase {
         System.out.println(employee1);
 
     }
+
+    /* send a get request to regions
+        verify that region ids are 1,2,3,4
+        verify that regions names Europe ,Americas , Asia, Middle East and Africa
+        verify that count is 4
+        try to use pojo as much as possible
+        ignore non used fields
+
+     */
 }
