877c50e8d88350692edb914106ce5a21bb353139
jamaldemir <jamal@cybertekschool.com>
2021-08-22 10:42:20 -0400

    Day6: Spartan.java added

---
+++ src/test/java/com/cybertek/pojo/Spartan.java
@@ -0,0 +1,61 @@
+package com.cybertek.pojo;
+
+
+/*
+{
+    "id": 15,
+    "name": "Meta",
+    "gender": "Female",
+    "phone": 1938695106
+}
+ */
+public class Spartan {
+        //getter setter
+        //toString
+        private int id;
+        private String name;
+        private String gender;
+        private long phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Spartan{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
