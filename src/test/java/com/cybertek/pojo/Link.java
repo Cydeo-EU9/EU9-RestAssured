7ac031d45a96f1016aa2d367dd328cfd16d6dac9
jamaldemir <jamal@cybertekschool.com>
2021-08-22 12:50:18 -0400

    Day6: lombok dependency added

--- src/test/java/com/cybertek/pojo/Link.java
+++ src/test/java/com/cybertek/pojo/Link.java
@@ -1,30 +1,15 @@
 package com.cybertek.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Link {
     private String rel;
     private String href;
 
-    @Override
-    public String toString() {
-        return "Link{" +
-                "rel='" + rel + '\'' +
-                ", href='" + href + '\'' +
-                '}';
-    }
 
-    public String getRel() {
-        return rel;
-    }
-
-    public void setRel(String rel) {
-        this.rel = rel;
-    }
-
-    public String getHref() {
-        return href;
-    }
-
-    public void setHref(String href) {
-        this.href = href;
-    }
 }
--- src/test/java/com/cybertek/pojo/Region.java
+++ src/test/java/com/cybertek/pojo/Region.java
@@ -1,42 +1,17 @@
 package com.cybertek.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class Region {
     private int region_id;
     private String region_name;
     private List<Link> links;
 
-    @Override
-    public String toString() {
-        return "Region{" +
-                "region_id=" + region_id +
-                ", region_name='" + region_name + '\'' +
-                ", links=" + links +
-                '}';
-    }
-
-    public int getRegion_id() {
-        return region_id;
-    }
-
-    public void setRegion_id(int region_id) {
-        this.region_id = region_id;
-    }
-
-    public String getRegion_name() {
-        return region_name;
-    }
-
-    public void setRegion_name(String region_name) {
-        this.region_name = region_name;
-    }
-
-    public List<Link> getLinks() {
-        return links;
-    }
-
-    public void setLinks(List<Link> links) {
-        this.links = links;
-    }
 }
