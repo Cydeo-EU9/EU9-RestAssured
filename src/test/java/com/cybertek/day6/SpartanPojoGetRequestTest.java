c06104ffda57cffd05941c8812009e512503019b
jamaldemir <jamal@cybertekschool.com>
2021-08-22 10:52:25 -0400

    Day6: Spartan.java first way added

--- src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
@@ -1,6 +1,9 @@
 package com.cybertek.day6;
 
+import com.cybertek.pojo.Spartan;
 import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
@@ -12,6 +15,20 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
     @Test
     public void oneSpartanPojo(){
 
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 15)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200)
+                .log().all()
+                .extract().response();
+
+        //De serialize --> JSON to POJO (java custom class)
+        //2 different way to do this
+        //1.using as() method
+        Spartan spartan15 = response.as(Spartan.class);
+        System.out.println(spartan15);
+        System.out.println("spartan15.getId() = " + spartan15.getId());
+        System.out.println("spartan15.getGender() = " + spartan15.getGender());
 
 
     }
