d7f39ab96978f29ff3b75ed26ed9cbc350b9268d
jamaldemir <jamal@cybertekschool.com>
2021-08-22 12:37:07 -0400

    Day6: Region.java and Link.java created

--- src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -1,6 +1,9 @@
 package com.cybertek.day6;
 
+import com.cybertek.pojo.Link;
+import com.cybertek.pojo.Region;
 import com.cybertek.utilities.HRTestBase;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,6 +15,18 @@ public class ORDSPojoGetRequestTest extends HRTestBase {
 
     @Test
     public void regionTest(){
+
+        JsonPath jsonPath = get("/regions").then().statusCode(200).extract().jsonPath();
+
+        Region region1 = jsonPath.getObject("items[0]", Region.class);
+
+        System.out.println(region1);
+
+        System.out.println("region1.getRegion_id() = " + region1.getRegion_id());
+        System.out.println("region1.getRegion_name() = " + region1.getRegion_name());
+        System.out.println("region1.getLinks().get(0).getHref() = " + region1.getLinks().get(0).getHref());
+
+
 
     }
 }
