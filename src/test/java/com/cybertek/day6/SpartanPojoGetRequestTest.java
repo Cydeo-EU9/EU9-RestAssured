c02e56bcb115e3efead8835e1676ef26d4b75bd7
jamaldemir <jamal@cybertekschool.com>
2021-08-22 10:45:22 -0400

    Day6: Spartan.java added

---
+++ src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
@@ -0,0 +1,18 @@
+package com.cybertek.day6;
+
+import com.cybertek.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class SpartanPojoGetRequestTest extends SpartanTestBase {
+
+    @DisplayName("GET one spartan and convert it to Spartan Object")
+    @Test
+    public void oneSpartanPojo(){
+
+
+
+    }
+}
--- src/test/java/com/cybertek/utilities/SpartanTestBase.java
+++ src/test/java/com/cybertek/utilities/SpartanTestBase.java
@@ -16,13 +16,13 @@ public abstract class SpartanTestBase {
         String dbUsername = "SP";
         String dbPassword = "SP";
 
-        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+        //DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
     }
 
     @AfterAll
     public static void teardown(){
 
-        DBUtils.destroy();
+        //DBUtils.destroy();
     }
 
 
