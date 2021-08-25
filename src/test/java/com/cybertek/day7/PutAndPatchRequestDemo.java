fcf5957f3b9391b96ce5e770151a3aa76a16844a
jamaldemir <jamal@cybertekschool.com>
2021-08-24 22:02:49 -0400

    Day7: PutAndPatchRequestDemo.java added

--- src/test/java/com/cybertek/day7/PutAndPatchRequestDemo.java
+++ src/test/java/com/cybertek/day7/PutAndPatchRequestDemo.java
@@ -1,12 +1,15 @@
 package com.cybertek.day7;
 
 import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class PutAndPatchRequestDemo extends SpartanTestBase {
 
 
@@ -17,12 +20,52 @@ public class PutAndPatchRequestDemo extends SpartanTestBase {
         Map<String,Object> putRequestMap = new LinkedHashMap<>();
         putRequestMap.put("name","BruceWayne");
         putRequestMap.put("gender","Male");
-        putRequestMap.put("phone",8877441111L);
+        putRequestMap.put("phone",8811111111L);
+
+        given().contentType(ContentType.JSON) //hey api I am sending JSON body
+                .body(putRequestMap).log().body()
+                .and().pathParam("id",388)
+                .when().put("/api/spartans/{id}")
+                .then()
+                .statusCode(204);
+
+        //send a GET request after update, make sure updated field changed, or the new info matching
+        //with requestBody that we send
+
+    }
+
+    @DisplayName("PATCH request to one spartan for partial update with Map")
+    @Test
+    public void PATCHRequest(){
+        //just like post request we have different options to send body, we will go with map
+        Map<String,Object> putRequestMap = new LinkedHashMap<>();
+        putRequestMap.put("phone",8811111111L);
+        putRequestMap.put("name","Peter");
+
+        given().contentType(ContentType.JSON) //hey api I am sending JSON body
+                .body(putRequestMap).log().body()
+                .and().pathParam("id",388)
+                .when().patch("/api/spartans/{id}")
+                .then()
+                .statusCode(204);
+
+        //send a GET request after update, make sure updated field changed, or the new info matching
+        //with requestBody that we send
 
 
+    }
+
+    @DisplayName("DELETE one spartan")
+    @Test
+    public void deleteSpartan(){
+        int idToDelete= 258;
 
 
+            given().pathParam("id",idToDelete)
+                    .when().delete("/api/spartans/{id}")
+                    .then().statusCode(204);
 
+            //send a get request after you delete make sure you are getting 404
 
     }
 
