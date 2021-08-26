8ee64d1be127a5c3b5344ad51af4f9cc545a1497
jamaldemir <jamal@cybertekschool.com>
2021-08-25 20:49:10 -0400

    Day8: new SpartanAuthTestBase.java and SpartanWithAuthTests.java admin test is done

--- src/test/java/com/cybertek/day8/SpartanWithAuthTests.java
+++ src/test/java/com/cybertek/day8/SpartanWithAuthTests.java
@@ -34,9 +34,34 @@ public class SpartanWithAuthTests extends SpartanAuthTestBase {
                 .statusCode(200)
                 .log().all();
 
-
-
     }
+
+    @DisplayName("DELETE /spartans/{id} as editor user expect 403")
+    @Test
+    public void testEditorDelete(){
+        given()
+                .auth().basic("editor","editor")
+                .and().accept(ContentType.JSON)
+                .and().pathParam("id",30)
+        .when()
+                .delete("/api/spartans/{id}")
+        .then()
+                .statusCode(403)
+                 .log().body();
+    }
+
+    /*
+        As a homework,write a detealied test for Role Base Access Control(RBAC)
+            in Spartan Auth app (7000)
+            Admin should be able take all CRUD
+            Editor should be able to take all CRUD
+                other than DELETE
+            User should be able to only READ data
+                not update,delete,create (POST,PUT,PATCH,DELETE)
+       --------------------------------------------------------
+        Can guest even read data ? 401 for all
+
+     */
 
 
 
