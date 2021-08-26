f03a11cbb014e8a0d4460dd0e5827bb4130252e5
jamaldemir <jamal@cybertekschool.com>
2021-08-25 20:56:37 -0400

    Day8: BookItTest.java completed

--- src/test/java/com/cybertek/day8/BookItTest.java
+++ src/test/java/com/cybertek/day8/BookItTest.java
@@ -1,5 +1,6 @@
 package com.cybertek.day8;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,10 +17,20 @@ public class BookItTest {
 
     }
 
+    String accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMzkiLCJhdWQiOiJzdHVkZW50LXRlYW0tbGVhZGVyIn0._vM1-eRoS7SsHu6T-QPdJoEdA8LSwnxUvvTTbhV-8ms";
+
     @DisplayName("GET all campuses")
     @Test
     public void testAuth1(){
 
+        given().
+                header("Authorization",accessToken)
+                .and().accept(ContentType.JSON)
+        .when()
+                .get("/api/campuses")
+        .then()
+                .statusCode(200)
+                .log().all();
 
 
 
