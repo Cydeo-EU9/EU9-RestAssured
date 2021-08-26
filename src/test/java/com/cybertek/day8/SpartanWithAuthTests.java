1ca2ae4e110a3256301936a0a6c6be60ca822824
jamaldemir <jamal@cybertekschool.com>
2021-08-25 20:39:59 -0400

    Day8: new SpartanAuthTestBase.java and SpartanWithAuthTests.java admin test is done

--- src/test/java/com/cybertek/day8/SpartanWithAuthTests.java
+++ src/test/java/com/cybertek/day8/SpartanWithAuthTests.java
@@ -1,4 +1,44 @@
 package com.cybertek.day8;
 
-public class SpartanWithAuthTests {
+import com.cybertek.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class SpartanWithAuthTests extends SpartanAuthTestBase {
+
+    @DisplayName("GET /api/spartans as a public user(guest) expect 401 ")
+    @Test
+    public void test1(){
+            given().accept(ContentType.JSON).
+                    when().
+            get("/api/spartans")
+                    .then().statusCode(401)
+                    .log().all();
+
+    }
+
+    @DisplayName("GET /api/spartans as admin user expect 200")
+    @Test
+    public void testAdmin(){
+        //how to pass admin admin as a username and password ?
+        given()
+                .auth().basic("admin","admin")
+                .and().accept(ContentType.JSON)
+        .when()
+                .get("/api/spartans")
+        .then()
+                .statusCode(200)
+                .log().all();
+
+
+
+    }
+
+
+
+
 }
