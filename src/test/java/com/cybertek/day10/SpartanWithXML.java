79f7bd51bb5aca7bb2b3a1c60971b01231479b2e
jamaldemir <jamal@cybertekschool.com>
2021-08-29 10:38:11 -0400

    Day10: SpartanWithXML.java test 2 completed

--- src/test/java/com/cybertek/day10/SpartanWithXML.java
+++ src/test/java/com/cybertek/day10/SpartanWithXML.java
@@ -2,9 +2,13 @@ package com.cybertek.day10;
 
 import com.cybertek.utilities.SpartanAuthTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -45,6 +49,20 @@ public class SpartanWithXML extends SpartanAuthTestBase {
                           .when()
                                   .get("/api/spartans");
 
+        //get response xml body/payload and save inside the xmlpath object
+        XmlPath xmlPath = response.xmlPath();
+
+
+        //get first spartan name
+        String name = xmlPath.getString("List.item[0].name");
+        System.out.println("name = " + name);
+        //get the 3rd spartan id number
+        int id = xmlPath.getInt("List.item[2].id");
+        System.out.println("id = " + id);
+
+        //how to get all names and save into list of string
+        List<String> names = xmlPath.getList("List.item.name");
+        System.out.println(names);
 
     }
 
