60d43face21af84b2f26ff229d31762c60bbd5ec
jamaldemir <jamal@cybertekschool.com>
2021-08-29 11:24:19 -0400

    Day10: govXmlTest.java added

---
+++ src/test/java/com/cybertek/day10/govXmlTest.java
@@ -0,0 +1,21 @@
+package com.cybertek.day10;
+
+import org.junit.jupiter.api.Test;
+
+public class govXmlTest {
+
+
+
+    @Test
+    public void test1(){
+        //send a get request to
+        //https://data.ct.gov/api/views/qm34-pq7e/rows.xml
+        //get all the years
+        //get all unknowns
+        //get 2006 other
+        //get 2007 _address
+
+
+
+    }
+}
