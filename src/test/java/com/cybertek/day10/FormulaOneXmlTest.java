d5ff3b7a34847f43453293950f73ad9ba93809a2
jamaldemir <jamal@cybertekschool.com>
2021-08-29 11:18:35 -0400

    Day10: FormulaOneXmlTest.java completed

--- src/test/java/com/cybertek/day10/FormulaOneXmlTest.java
+++ src/test/java/com/cybertek/day10/FormulaOneXmlTest.java
@@ -1,5 +1,6 @@
 package com.cybertek.day10;
 
+import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -27,6 +28,24 @@ public class FormulaOneXmlTest {
                              .then().statusCode(200).log().all()
                                     .extract().response();
 
+        XmlPath xmlPath = response.xmlPath();
+        //get given name
+        String givenName = xmlPath.getString("MRData.DriverTable.Driver.GivenName");
+        System.out.println("givenName = " + givenName);
+        //get family name
+        String familyName = xmlPath.getString("MRData.DriverTable.Driver.FamilyName");
+        System.out.println("familyName = " + familyName);
+        //if you are trying to get attribute we use @ sign
+        //get driverId
+        String driverId = xmlPath.getString("MRData.DriverTable.Driver.@driverId");
+        System.out.println("driverId = " + driverId);
+
+        //get code
+        String code = xmlPath.getString("MRData.DriverTable.Driver.@code");
+        System.out.println("code = " + code);
+        //get url
+        String url = xmlPath.getString("MRData.DriverTable.Driver.@url");
+        System.out.println("url = " + url);
 
     }
 }
