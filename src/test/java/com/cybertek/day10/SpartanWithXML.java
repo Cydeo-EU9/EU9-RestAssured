9271b10b033f4dcd7c02c66788b9647ae3a99c3b
jamaldemir <jamal@cybertekschool.com>
2021-08-29 10:12:53 -0400

    Day10: SpartanWithXML.java added

---
+++ src/test/java/com/cybertek/day10/SpartanWithXML.java
@@ -0,0 +1,17 @@
+package com.cybertek.day10;
+
+import com.cybertek.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SpartanWithXML extends SpartanAuthTestBase {
+
+    @DisplayName("GET request to /api/spartans and verify xml")
+    @Test
+    public void getSpartanXml(){
+
+
+
+    }
+
+}
--- src/test/java/com/cybertek/utilities/SpartanAuthTestBase.java
+++ src/test/java/com/cybertek/utilities/SpartanAuthTestBase.java
@@ -8,7 +8,7 @@ public abstract class SpartanAuthTestBase {
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://52.207.61.129:7000";
+        baseURI = "http://44.195.19.167:7000";
 
     }
 }
