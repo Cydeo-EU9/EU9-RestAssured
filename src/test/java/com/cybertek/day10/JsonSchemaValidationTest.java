8baf11f731520f8639eb47ffca1a2fb0356bd1e2
jamaldemir <jamal@cybertekschool.com>
2021-08-29 12:48:11 -0400

    Day10: JsonSchemaValidationTest.java added

---
+++ src/test/java/com/cybertek/day10/JsonSchemaValidationTest.java
@@ -0,0 +1,23 @@
+package com.cybertek.day10;
+
+import com.cybertek.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class JsonSchemaValidationTest extends SpartanAuthTestBase {
+
+    @DisplayName("GET request to verify one spartan against to schema")
+    @Test
+    public void schemaValidation(){
+
+
+
+
+
+
+    }
+
+}
