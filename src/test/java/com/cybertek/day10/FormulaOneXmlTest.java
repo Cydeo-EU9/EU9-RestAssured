1a6ddfcea619a410affddfd3ff5590eef0e41b15
jamaldemir <jamal@cybertekschool.com>
2021-08-29 10:53:30 -0400

    Day10: FormulaOneXmlTest.java added

--- src/test/java/com/cybertek/day10/FormulaOneXmlTest.java
+++ src/test/java/com/cybertek/day10/FormulaOneXmlTest.java
@@ -1,6 +1,9 @@
 package com.cybertek.day10;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -8,7 +11,21 @@ public class FormulaOneXmlTest {
 
     @BeforeAll
     public static void setUp(){
+        //http://ergast.com/api/f1/drivers/alonso
         baseURI = "http://ergast.com";
+        basePath ="/api/f1";
+
+    }
+
+    @Test
+    public void test1(){
+
+        Response response = given()
+                                 .pathParam("driver", "alonso")
+                             .when()
+                                     .get("/drivers/{driver}")
+                             .then().statusCode(200).log().all()
+                                    .extract().response();
 
 
     }
