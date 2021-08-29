0aba8c33f3896b09122d079ca934f2a0c9680a73
jamaldemir <jamal@cybertekschool.com>
2021-08-29 14:21:02 -0400

    Day10: schema test 2 added

--- src/test/java/com/cybertek/day10/JsonSchemaValidationTest.java
+++ src/test/java/com/cybertek/day10/JsonSchemaValidationTest.java
@@ -6,6 +6,8 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -29,5 +31,23 @@ public class JsonSchemaValidationTest extends SpartanAuthTestBase {
                     .log().all();
 
     }
+
+    @DisplayName("GET request to all spartans and verify schema")
+    @Test
+    public void allSpartanSchemaTest(){
+
+        given()
+                .accept(ContentType.JSON)
+                .auth().basic("admin","admin")
+        .when()
+                .get("/api/spartans")
+        .then()
+                .statusCode(200)
+                //what if you have your .json file not under resources following way -->
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/com/cybertek/day10/allSpartansSchema.json")));
+
+    }
+
+
 
 }
