1fdc36f466d28e7353e005cb0d2130f2410463b0
jamaldemir <jamal@cybertekschool.com>
2021-08-29 15:18:32 -0400

    Day10: ResponseTimeTest.java added

---
+++ src/test/java/com/cybertek/day10/ResponseTimeTest.java
@@ -0,0 +1,21 @@
+package com.cybertek.day10;
+
+import com.cybertek.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class ResponseTimeTest extends SpartanAuthTestBase {
+
+
+    @Test
+    public void test1(){
+
+
+
+
+
+
+
+    }
+}
