9d0a24c8277235a8d41a484b87873eaf27242a99
jamaldemir <jamal@cybertekschool.com>
2021-08-31 19:17:43 -0400

    Day11: TestLifeCycleAnnotations.java added

--- src/test/java/com/cybertek/day10/ResponseTimeTest.java
+++ src/test/java/com/cybertek/day10/ResponseTimeTest.java
@@ -1,6 +1,8 @@
 package com.cybertek.day10;
 
 import com.cybertek.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,10 +13,16 @@ public class ResponseTimeTest extends SpartanAuthTestBase {
     @Test
     public void test1(){
 
+        Response response = given()
+                .auth().basic("admin", "admin")
+                .accept(ContentType.JSON)
+                .when()
+                .get("/api/spartans")
+                .then()
+                .time(both(greaterThan(500L)).and(lessThanOrEqualTo(1100L)))
+                .extract().response();
 
-
-
-
+        System.out.println("response.getTime() = " + response.getTime());
 
 
     }
