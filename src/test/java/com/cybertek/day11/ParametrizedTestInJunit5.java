317c94c605c4d865b1e33a23ca9bc4dba02b4f33
jamaldemir <jamal@cybertekschool.com>
2021-08-31 19:36:57 -0400

    Day11: ParametrizedTestInJunit5.java 2 tests are done

--- src/test/java/com/cybertek/day11/ParametrizedTestInJunit5.java
+++ src/test/java/com/cybertek/day11/ParametrizedTestInJunit5.java
@@ -1,9 +1,27 @@
 package com.cybertek.day11;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class ParametrizedTestInJunit5 {
 
 
+        @ParameterizedTest
+        @ValueSource(ints = {1,3,5,6,7,9,10,5,21,32})
+        public void testMultipleNumbers(int number){
 
+         System.out.println("number = " + number);
+                Assertions.assertTrue(number > 5);
+
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"john","abbas","ali","TJ"})
+        public void testMultipleNames(String name){
+            System.out.println("name = " + name);
+
+        }
 
 
 
