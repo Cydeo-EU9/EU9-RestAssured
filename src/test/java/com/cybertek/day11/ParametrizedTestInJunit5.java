30cd6bc36ac0da2f059d1c9f6834a3e61b1595a2
jamaldemir <jamal@cybertekschool.com>
2021-08-31 19:54:38 -0400

    Day11: ParametrizedTestInJunit5.java zipcode completed

--- src/test/java/com/cybertek/day11/ParametrizedTestInJunit5.java
+++ src/test/java/com/cybertek/day11/ParametrizedTestInJunit5.java
@@ -3,7 +3,9 @@ package com.cybertek.day11;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class ParametrizedTestInJunit5 {
 
 
@@ -27,6 +29,20 @@ public class ParametrizedTestInJunit5 {
     // with these zipcodes 22030,22031, 22032, 22033 , 22034, 22035, 22036
     // check status code 200
 
+    @ParameterizedTest
+    @ValueSource(ints = {22030,22031, 22032, 22033 , 22034, 22035, 22036})
+    public void zipCodeTest(int zipCode){
+
+            given()
+                    .baseUri("https://api.zippopotam.us")
+                    .pathParam("zipcode",zipCode) //we got zipcode from valueSource
+                    .log().all() //request log
+            .when()
+                    .get("/us/{zipcode}")
+            .then().log().all() //response log
+                    .statusCode(200);
+
+    }
 
 
 }
