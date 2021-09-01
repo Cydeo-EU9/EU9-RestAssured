85c96995fe977d166b101fd0ec52598358672f19
jamaldemir <jamal@cybertekschool.com>
2021-08-31 21:20:02 -0400

    Day11: CsvFileSourceParametrizedTest completed

--- src/test/java/com/cybertek/day11/CsvFileSourceParametrizedTest.java
+++ src/test/java/com/cybertek/day11/CsvFileSourceParametrizedTest.java
@@ -1,7 +1,13 @@
 package com.cybertek.day11;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class CsvFileSourceParametrizedTest {
 
@@ -15,6 +21,16 @@ public class CsvFileSourceParametrizedTest {
         System.out.println("cityArg = " + cityArg);
         System.out.println("zipCountArg = " + zipCountArg);
         //send a request and verify place number matches with zipCount
-
+        given()
+                .baseUri("https://api.zippopotam.us")
+                .accept(ContentType.JSON)
+                .pathParam("state", stateArg)
+                .pathParam("city", cityArg)
+                .log().uri()
+                .when()
+            .get("/us/{state}/{city}")
+            .then()
+                .statusCode(200)
+                .body("places",hasSize(zipCountArg));
     }
 }
