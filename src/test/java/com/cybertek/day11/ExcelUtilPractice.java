1906c3be77bbb4a4116df01d1fbf8aae5e687c10
jamaldemir <jamal@cybertekschool.com>
2021-08-31 21:34:51 -0400

    Day11: Vytracktestdata.xlsx added

--- src/test/java/com/cybertek/day11/ExcelUtilPractice.java
+++ src/test/java/com/cybertek/day11/ExcelUtilPractice.java
@@ -1,4 +1,26 @@
 package com.cybertek.day11;
 
+import com.cybertek.utilities.ExcelUtil;
+import org.junit.jupiter.api.Test;
+
 public class ExcelUtilPractice {
+
+
+
+    @Test
+    public void test1(){
+
+        //How to use excelUtil file ?
+        //it accepts two arguments
+        //Argument 1: location of the file(path)
+        //Argument 2: sheet that we want to open
+        ExcelUtil vytrackFile = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx","QA3-short");
+
+
+
+
+
+
+    }
+
 }
