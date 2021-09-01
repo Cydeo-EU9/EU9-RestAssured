27ae3d8f0931488751f55aa922756168166c5777
jamaldemir <jamal@cybertekschool.com>
2021-09-01 19:22:08 -0400

    Day12: OldRestAssuredTest.java test 1 added

--- src/test/java/com/cybertek/day12/OldRestAssuredTest.java
+++ src/test/java/com/cybertek/day12/OldRestAssuredTest.java
@@ -1,4 +1,31 @@
 package com.cybertek.day12;
 
-public class OldRestAssuredTest {
+import com.cybertek.utilities.SpartanNewBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class OldRestAssuredTest extends SpartanNewBase {
+
+
+    @Test
+    public void getAllSpartan(){
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .log().all()
+        .when()
+                .get("/spartans")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id[0]",is(1))
+                .log().all();
+
+    }
+
+
 }
--- src/test/java/com/cybertek/utilities/SpartanNewBase.java
+++ src/test/java/com/cybertek/utilities/SpartanNewBase.java
@@ -1,15 +1,23 @@
 package com.cybertek.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
-import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
 
 public class SpartanNewBase {
 
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://44.195.19.167:7000";
+        baseURI = "http://44.195.19.167";
+        port = 7000;
+        basePath ="/api";
+    }
 
+    @AfterAll
+    public static void close(){
+        //reset the info we set above ,method comes from restassured
+        reset();
     }
 }
