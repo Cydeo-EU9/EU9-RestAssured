ea8789584123b9a0ff32d396187b0d5a82fdd7e7
jamaldemir <jamal@cybertekschool.com>
2021-08-31 20:16:53 -0400

    Day11: CsvSourceParametrizedTest.java added

--- src/test/java/com/cybertek/day11/CsvSourceParametrizedTest.java
+++ src/test/java/com/cybertek/day11/CsvSourceParametrizedTest.java
@@ -1,5 +1,8 @@
 package com.cybertek.day11;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CsvSourceParametrizedTest {
 
     // Test first number + second number = third number
@@ -7,5 +10,15 @@ public class CsvSourceParametrizedTest {
 //            2, 3 , 5
 //            8, 7 , 15
 //            10, 9 , 19
+
+    @ParameterizedTest
+    @CsvSource({"1, 3 , 4",
+                "2, 3 , 5",
+                "8, 7 , 15",
+                "10, 9 , 19"})
+    public void testAddition(int num1,int num2, int sum){
+
+
+    }
 
 }
