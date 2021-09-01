df1d4059e7320dec4eefb6b9969742b2844dee53
jamaldemir <jamal@cybertekschool.com>
2021-08-31 20:13:46 -0400

    Day11: CsvSourceParametrizedTest.java added

---
+++ src/test/java/com/cybertek/day11/CsvSourceParametrizedTest.java
@@ -0,0 +1,11 @@
+package com.cybertek.day11;
+
+public class CsvSourceParametrizedTest {
+
+    // Test first number + second number = third number
+//            1, 3 , 4
+//            2, 3 , 5
+//            8, 7 , 15
+//            10, 9 , 19
+
+}
