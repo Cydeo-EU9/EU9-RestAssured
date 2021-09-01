a1df533e26948f5d9c5120e4c83a1fbbc080b307
jamaldemir <jamal@cybertekschool.com>
2021-08-31 20:25:08 -0400

    Day11: CsvSourceParametrizedTest.java task added

--- src/test/java/com/cybertek/day11/CsvSourceParametrizedTest.java
+++ src/test/java/com/cybertek/day11/CsvSourceParametrizedTest.java
@@ -2,7 +2,9 @@ package com.cybertek.day11;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class CsvSourceParametrizedTest {
 
     // Test first number + second number = third number
@@ -18,7 +20,27 @@ public class CsvSourceParametrizedTest {
                 "10, 9 , 19"})
     public void testAddition(int num1,int num2, int sum){
 
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("sum = " + sum);
+        //assert num1+ num2 equalsto sum
 
+        assertThat(num1+num2,equalTo(sum));
     }
+
+    // Write a parameterized test for this request
+    // GET https://api.zippopotam.us/us/{state}/{city}
+    /*
+        "NY, New York",
+        "CO, Denver",
+        "VA, Fairfax",
+        "VA, Arlington",
+        "MA, Boston",
+        "NY, New York",
+        "MD, Annapolis"
+     */
+    //verify place name contains your city name
+    //print number of places for each request
+
 
 }
