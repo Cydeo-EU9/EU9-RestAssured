d887c522ba397032966cbcd2351683f65e07cc1b
jamaldemir <jamal@cybertekschool.com>
2021-09-01 20:32:43 -0400

    Day12: BookitSpecTest.java and BookitTestBase.java added

--- src/test/java/com/cybertek/day12/BookitSpecTest.java
+++ src/test/java/com/cybertek/day12/BookitSpecTest.java
@@ -1,6 +1,29 @@
 package com.cybertek.day12;
 
 import com.cybertek.utilities.BookitTestBase;
+import org.junit.jupiter.api.Test;
 
 public class BookitSpecTest extends BookitTestBase {
+
+    @Test
+    public void test1(){
+        //send a get request to /api/users/me endpoint as a teacher
+        //verify status code and content type
+
+
+
+
+
+    }
+
+    @Test
+    public void test2(){
+        //send a get request to /api/users/me endpoint as a student-member
+        //verify status code and content type
+
+
+
+
+
+    }
 }
--- src/test/java/com/cybertek/utilities/BookitTestBase.java
+++ src/test/java/com/cybertek/utilities/BookitTestBase.java
@@ -1,4 +1,30 @@
 package com.cybertek.utilities;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.*;
+
 public class BookitTestBase {
+
+    @BeforeAll
+    public static void init(){
+      baseURI = ConfigurationReader.getProperty("qa3_api_url");
+
+
+    }
+
+    @AfterAll
+    public static void teardown(){
+        reset();
+    }
+
 }
