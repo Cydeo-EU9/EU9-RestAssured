36fde76af62ce4cd5ad042f6639b1a6a618a0bb3
jamaldemir <jamal@cybertekschool.com>
2021-09-01 20:37:17 -0400

    Day12: BookitTestBase.java task added

--- src/test/java/com/cybertek/day12/BookitSpecTest.java
+++ src/test/java/com/cybertek/day12/BookitSpecTest.java
@@ -2,14 +2,17 @@ package com.cybertek.day12;
 
 import com.cybertek.utilities.BookitTestBase;
 import org.junit.jupiter.api.Test;
-
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class BookitSpecTest extends BookitTestBase {
 
     @Test
     public void test1(){
         //send a get request to /api/users/me endpoint as a teacher
         //verify status code and content type
-
+            given()
+                    .spec(teacherReqSpec)
 
 
 
--- src/test/java/com/cybertek/utilities/BookitTestBase.java
+++ src/test/java/com/cybertek/utilities/BookitTestBase.java
@@ -14,17 +14,34 @@ import org.junit.jupiter.api.BeforeAll;
 import static io.restassured.RestAssured.*;
 
 public class BookitTestBase {
+    public static RequestSpecification teacherReqSpec;
+    public static RequestSpecification studentMemberReqSpec;
+    public static RequestSpecification studentLeaderReqSpec;
 
     @BeforeAll
     public static void init(){
       baseURI = ConfigurationReader.getProperty("qa3_api_url");
 
+        teacherReqSpec = given()
+                                .accept(ContentType.JSON)
+                                .header("Authorization","teacherTokenHere")
 
     }
 
     @AfterAll
     public static void teardown(){
         reset();
+    }
+
+    //teacher , student-member,student-leader
+    //it will take user info from conf.properties
+    public static String getTokenByRole(String role){
+        //switch,if make sure you get correct user info
+        //send request/get token/ return token
+
+
+
+
     }
 
 }
