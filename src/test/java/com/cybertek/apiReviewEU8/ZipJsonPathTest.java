e5d21d1fa1f36b7a5f4d40709a5e3e79e7f8ccec
OscarSah <oscar@cybertekschool.com>
2022-07-23 19:26:26 +0300

    Zip Request Task Shared

--- src/test/java/com/cybertek/apiReviewEU8/ZipJsonPathTest.java
+++ src/test/java/com/cybertek/apiReviewEU8/ZipJsonPathTest.java
@@ -1,5 +1,16 @@
 package com.cybertek.apiReviewEU8;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 public class ZipJsonPathTest extends ZipBase{
         /*
     Exercise with JsonPath Method
@@ -19,4 +30,26 @@ And body should contains following information
    latitudes are 42.4464,42.4128
 
     */
+    @Test
+    public void test(){
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("state","ma")
+                .and().pathParam("city","belmont")
+                .when()
+                .get("/{state}/{city}");
+        response.prettyPrint();
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.contentType());
+        assertTrue(response.header("Server").equalsIgnoreCase("cloudflare"));
+        assertTrue(response.headers().hasHeaderWithName("Report-To"));
+
+        // for JSONPATH create object
+        JsonPath jsonPath = response.jsonPath();
+        assertEquals("United States",jsonPath.getString("country"));
+
+        List<String> expectedZips = new ArrayList<>(Arrays.asList("02178","02478"));
+        List<String> actualZips = jsonPath.getList("places.\'post code\'");
+        assertEquals(expectedZips,actualZips);
+    }
+
 }
